439eb9fa0b930d11bce428ba2faf36b4332125b7
Diego-Capu <diegoospino487@gmail.com>
2026-03-25 21:06:05 -0500

    Please enter the commit message for your changes. Lines starting

---
+++ app.py
@@ -0,0 +1,30 @@
+from servicios import agregar_producto,mostrar_producto,calcular_estadisticas
+
+
+inventario = []
+
+def menu_principal():
+    while True:
+        print("Menu principal")
+        print("1.Agregar producto")
+        print("2.Mostrar producto")
+        print("3.Calcular estadisticas")
+        print("Salir")
+        option = int(input("Ingrese la opcion que desea realizar: "))
+
+        if option == 1:
+            agregar_producto()
+    
+           
+        elif option == 2:
+            mostrar_producto()
+            
+        elif option == 3:
+            calcular_estadisticas()
+            
+        elif option == 4:
+            break
+        
+
+if __name__ == "__main__":
+    menu_principal()
--- servicios.py
+++ servicios.py
@@ -10,13 +10,7 @@ def agregar_producto():
     producto = {"nombre": nombre, "precio": precio, "cantidad": cantidad}
     inventario.append(producto)
     print("Agregado exitosamente")
-
-def mostrar_producto():
-    if not inventario:
-        print("")
-    else:
-        for l in (inventario):
-            print(f"Nombre: {l['nombre']}, Precio: {l['precio']}, {l['cantidad']}")
+    return
 
 def calcular_estadisticas():
     print("Estadistica inventario")
@@ -28,25 +22,12 @@ def calcular_estadisticas():
 
         print(f"valor total del inventario: ${total_valor}")
         print(f"cantidad de tipos de productos registrados: {total_productos} ")
-
-def menu_principal():
-    while True:
-        print("Menu principal")
-        print("1.Agregar producto")
-        print("2.Mostrar producto")
-        print("3.Calcular estadisticas")
-        print("Salir")
-        option = int(input("Ingrese la opcion que desea realizar: "))
-
-        if option == 1:
-            agregar_producto()
-        if option == 2:
-            mostrar_producto()
-        if option == 3:
-            calcular_estadisticas()
-        if option == 4:
-            break
-        
-
-if __name__ == "__main__":
-    menu_principal()
+        return
+    
+def mostrar_producto():
+    if not inventario:
+        print("")
+    else:
+        for l in (inventario):
+            print(f"Nombre: {l['nombre']}, Precio: {l['precio']}, {l['cantidad']}")
+            return
